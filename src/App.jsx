b320d0daee30b169a7d843fd7cad0291e92070ec
mamadu jalloh <mamadujalloh>
2025-08-21 18:08:58 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const name = "Mamadu Jalloh"
+ 
+  return (
+    <>
+     <h1>Hello {name}</h1>
+    </>
+  )
+}
+
+export default App
